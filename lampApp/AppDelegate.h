ee1307b81aacd17efaa9bb0285c7b06c5d2eae43
冯景安先生 <apple@fengjinganxiansheng.local>
2015-11-01 01:01:19 +0800

    Initial Commit

---
+++ lampApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  lampApp
+//
+//  Created by 冯景安先生 on 15/11/1.
+//  Copyright © 2015年 anan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ lampApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  lampApp
+//
+//  Created by 冯景安先生 on 15/11/1.
+//  Copyright © 2015年 anan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
